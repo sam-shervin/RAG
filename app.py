09f98e6d6c184690ff3c1a2da121ba716d79f423
sam-shervin <samshervin.sebastian@gmail.com>
2024-07-10 05:26:50 +0530

    optimization

--- app.py
+++ app.py
@@ -4,57 +4,34 @@ import ollama
 import chromadb
 import numpy as np
 
-
 # Function to compute the cosine similarity between two vectors
 def compute_cosine_similarity(u: np.ndarray, v: np.ndarray) -> float:
     return (u @ v) / (np.linalg.norm(u) * np.linalg.norm(v))
 
+# Load PDFs and split them into chunks
+docs = RecursiveCharacterTextSplitter(
+    chunk_size=600,
+    chunk_overlap=300,
+    length_function=len,
+    is_separator_regex=False,
+).split_documents(PyPDFDirectoryLoader("data").load())
 
-# Load the PDFs from the data directory
-pdf_loader = PyPDFDirectoryLoader("data").load()
-
-# Split the PDFs into chunks of 800 characters with an overlap of 80 characters
-text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=600,
-        chunk_overlap=300,
-        length_function=len,
-        is_separator_regex=False,
-    )
-docs = text_splitter.split_documents(pdf_loader)
-
+# Create a ChromaDB collection and store document embeddings
 client = chromadb.Client()
 collection = client.create_collection(name="docs")
+for d in docs:
+    embedding = ollama.embeddings(model="nomic-embed-text", prompt=d.page_content)["embedding"]
+    collection.add(ids=[str(d.metadata)], embeddings=[embedding], documents=[d.page_content])
 
-# store each document in a vector embedding database
-for i, d in enumerate(docs):
-  response = ollama.embeddings(model="nomic-embed-text", prompt=d.page_content)
-  embedding = response["embedding"]
-  collection.add(
-    ids=[str(d.metadata)],
-    embeddings=[embedding],
-    documents=[d.page_content]
-  )
-
-
+# Query the most relevant document for the given prompt
 prompt = "How to install Astro?"
-
-# generate an embedding for the prompt and retrieve the most relevant doc
-response = ollama.embeddings(
-  prompt=prompt,
-  model="nomic-embed-text"
-)
-results = collection.query(
-  query_embeddings=[response["embedding"]],
-  n_results=4,
-)
-
-#print("similarity: ",compute_cosine_similarity(np.array(response["embedding"]), np.array(results["embeddings"][0][0])))
-
+query_embedding = ollama.embeddings(model="nomic-embed-text", prompt=prompt)["embedding"]
+results = collection.query(query_embeddings=[query_embedding], n_results=4)
 context = str(results["documents"])
-# get the most relevant document and generate a response
-output = ollama.generate(
-  model="llama3-gradient",
-  prompt=f"Using this data: {context}. Respond to this prompt: {prompt}"
-)
+
+# Generate a response using the most relevant document context
+output = ollama.generate(model="llama3-gradient", prompt=f"Using this data: {context}. Respond to this prompt: {prompt}")
 
 print(output['response'])
+
+#print("similarity: ",compute_cosine_similarity(np.array(response["embedding"]), np.array(results["embeddings"][0][0])))
