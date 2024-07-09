9dea9e56612ff29fa9e32b81c7a2a202717946ec
sam-shervin <samshervin.sebastian@gmail.com>
2024-07-10 03:30:12 +0530

    split the pdf content into chunks

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-from langchain.document_loaders.pdf import PyPDFDirectoryLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-# Load all PDFs in the data directory
+# Load the PDFs from the data directory
 pdf_loader = PyPDFDirectoryLoader("data").load()
 
+# Split the PDFs into chunks of 800 characters with an overlap of 80 characters
+text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=600,
+        chunk_overlap=300,
+        length_function=len,
+        is_separator_regex=False,
+    )
+print(text_splitter.split_documents(pdf_loader))
